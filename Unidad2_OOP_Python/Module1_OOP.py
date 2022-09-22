0fbc387c0c4badbeb2d7fd9a49a51a38293dc365
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-09-22 18:53:20 -0500

    Update Module1_OOP.py

--- Unidad2_OOP_Python/Module1_OOP.py
+++ Unidad2_OOP_Python/Module1_OOP.py
@@ -1,1 +1,25 @@
+class DataLoader:
 
+    #atributo de clase, compartido entre todas las instancias
+    #de la clase
+    loads = 0
+
+    #método de inicialización, invocado al crear un objeto
+    def __init__(self, pFileName):
+        
+        #loads es un atributo de clase:
+        DataLoader.loads += 1
+
+        #fileName es una atributo de instancia
+        self.fileName = pFileName
+
+    def loadCSV():
+        pass
+
+    def loadJSON():
+        pass
+
+    def loadXML():
+        pass
+
+    
