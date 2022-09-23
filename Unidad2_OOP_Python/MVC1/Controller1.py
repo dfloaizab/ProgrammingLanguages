1e5c761a340e69e718f00553173b5217fb39f908
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-09-22 20:44:34 -0500

    Create Controller1.py

---
+++ Unidad2_OOP_Python/MVC1/Controller1.py
@@ -0,0 +1,15 @@
+class DataController:
+
+
+    def __init__(self, pModel, pView):
+        self.model
+        self.view = pView
+
+    
+    #método que va a ser consumido por la vista
+    def consumeData(self):
+        pass
+
+    #método que va a notificar al modelo, generando cambios
+    def changeData(self):
+        pass
