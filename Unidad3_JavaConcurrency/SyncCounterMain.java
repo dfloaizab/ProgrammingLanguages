afadf2d2c9293b2d58f194c3afac478bf8a3d809
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-11-10 21:01:52 -0500

    Create SyncCounterMain.java

---
+++ Unidad3_JavaConcurrency/SyncCounterMain.java
@@ -0,0 +1,33 @@
+
+
+
+public class SyncCounterMain implements Runnable {
+	
+	static SyncCounter mySyncCounter;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Thread.currentThread().getName());
+		
+		mySyncCounter.increment();
+		mySyncCounter.increment();
+		mySyncCounter.increment();
+		mySyncCounter.decrement();
+		mySyncCounter.decrement();
+		mySyncCounter.decrement();
+		mySyncCounter.decrement();
+		mySyncCounter.decrement();		
+	}
+		
+	public static void main(String args[])
+	{
+		mySyncCounter = new SyncCounter();
+		
+		Thread t1 = new Thread(new SyncCounterMain());
+		Thread t2 = new Thread(new SyncCounterMain());		
+		
+	}
+
+}
