7018ad9c9e8749978a888b1fe22425d84c4ccb5f
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-11-17 19:14:05 -0500

    Create SynchronizedCounter.java

---
+++ Unidad3_JavaConcurrency/SynchronizedCounter.java
@@ -0,0 +1,16 @@
+public class SynchronizedCounter {
+  
+    private int c = 0;
+
+    public synchronized void increment() {
+        c++;
+    }
+
+    public synchronized void decrement() {
+        c--;
+    }
+
+    public synchronized int value() {
+        return c;
+    }
+}
