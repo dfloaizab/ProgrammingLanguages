db35fcf11e518efe7e94c8682c3b11ec91d4ec16
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-11-17 19:53:52 -0500

    Update SynchronizedCounter.java

--- Unidad3_JavaConcurrency/SynchronizedCounter.java
+++ Unidad3_JavaConcurrency/SynchronizedCounter.java
@@ -1,9 +1,16 @@
 public class SynchronizedCounter {
   
-    private int c = 0;
-
-    public synchronized void increment() {
-        c++;
+    //se indica que la variable puede ser afectada por varios hilos
+    private volatile int c = 0;
+  
+    private Object lock1 = new Object();
+  
+    public void increment() {
+            
+        synchronized(lock1){
+          c++;
+        }
+      
     }
 
     public synchronized void decrement() {
