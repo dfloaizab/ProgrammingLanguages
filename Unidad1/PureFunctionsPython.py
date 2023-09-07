3e06aa3e5e8de7b93ed947d31c82f43ebc2bc763
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-09-07 15:08:01 -0500

    Update PureFunctionsPython.py

--- Unidad1/PureFunctionsPython.py
+++ Unidad1/PureFunctionsPython.py
@@ -1,1 +1,34 @@
 
+#Una función pura en python (no modifica el parámetro de entrada, 
+# crea una nueva variable)
+#entrada: una lista de enteros
+#salida: una COPIA de la lista de entrada, con sus valores ^2
+def unaFuncionPura(listaDeEntrada):
+    #una lista vacía en python:
+    listaSalida = []
+    #iterando en una lista:
+    for x in listaDeEntrada:
+        #agregando un elemento a una lista:
+        listaSalida.append(x**2)
+
+    return listaSalida
+
+def convertirAMayusculas(listaEntrada):
+    listaNueva = []
+    for nombre in listaEntrada:
+        listaNueva.append(str(nombre).upper())
+    return listaNueva
+
+def convertirAMinusculas(listaEntrada):
+    listaNueva = []
+    for nombre in listaEntrada:
+        listaNueva.append(str(nombre).lower())
+    return listaNueva
+
+
+
+
+
+
+    
+
