c60761d2b873fa423d82bf9b20891fb6d71a0cef
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-11-09 19:20:00 -0500

    Create MainClass.java

---
+++ Unidad3_JavaConcurrency/AccountSample_2023B/MainClass.java
@@ -0,0 +1,15 @@
+
+public class MainClass {
+	
+	public static void main(String[] args)
+	{
+		Account myAccount = new Account(50000000);
+		
+		//operaciones secuenciales:
+		
+		
+		myAccount.deposit(500000);
+		myAccount.withdraw(40000);
+	}
+
+}
