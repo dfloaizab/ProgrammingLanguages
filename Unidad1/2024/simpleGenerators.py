17d1f08bc004c6100ea7e9c52ddcadcd85d10237
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2024-03-14 17:47:06 -0500

    Update simpleGenerators.py

--- Unidad1/2024/simpleGenerators.py
+++ Unidad1/2024/simpleGenerators.py
@@ -4,12 +4,17 @@ def gen1():
 
 gen_fun = gen1()
 
+#obtener primeros 2 valores:
 print(next(gen1()))
 print(next(gen1()))
 
 print("de ciclo:\n")
 try:
-    for i in range(1,101):
+    #obtener próximos números:
+    for i in range(1,80):
         print(next(gen_fun))
+    #obtener los próximos 10 con list comprehension:
+    some_more = [next(gen1()) for _ in range(10)]
+
 except StopIteration:
     pass
