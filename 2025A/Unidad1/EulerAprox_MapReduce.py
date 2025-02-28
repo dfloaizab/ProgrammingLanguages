698218d1a13a147c4fda8781aec75769d655d48e
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-02-28 11:11:13 -0500

    Update EulerAprox_MapReduce.py

--- 2025A/Unidad1/EulerAprox_MapReduce.py
+++ 2025A/Unidad1/EulerAprox_MapReduce.py
@@ -6,29 +6,32 @@ lim = int(input("Ingrese el numero de términos:"))
 nums = list(range(0,lim+1))
 print(nums)
 
-#función para calcular cada término de la serie de McLaurin que aproxima e:
-def fun_map(n):
+#función para calcular cada término de la serie de McLaurin que aproxima e1:
+def fun_map_e1(n):
     return (1/factorial(n))
 
-def fun_map_1(n, x):
+#función para calcular cada término de la serie de Maclaurin para la aproximación de e^x:
+def fun_map_ex(n, x):
     return (x**n/factorial(n))
 
 #función que calcula la sumatoria de toda la lista sumando los términos por pares:
 def fun_redux(a,b):
     return a+b
 
+#--- Aproximación de e^1 con map-reduce:
 #aplica la función dentro de la sumatoria a cada término de 0 a lim:
-terminos1 =  list(map(fun_map, nums))
-
+terminos1 =  list(map(fun_map_e1, nums))
 #aproxima e^1 sumando todos los términos:
 aprox = reduce(  fun_redux, terminos1 )
 
 print(f"Aproximación de e^1 con {lim} términos:{aprox}")
 
 #cómo aproximar e^x?
+#--- Aproximación de e^x con map-reduce:
 x = int(input("Ingrese x:"))
-
-terminos2 =  list(map(lambda n:fun_map_1(n,x), nums))
+#calcula cada término de la serie con map:
+terminos2 =  list(map(lambda n:fun_map_ex(n,x), nums))
+#reduce usando la suma con reduce:
 aprox2 = reduce(fun_redux, terminos2)
 
 print(f"Aproximación de e^{x}:{aprox2}")
